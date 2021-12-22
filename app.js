73fa4f41d4bea8f39ff085b1902500550a68076b
ganchock <ganchock@yahoo.com>
2021-12-22 23:07:46 +0800

    making changes in players

--- app.js
+++ app.js
@@ -9,24 +9,51 @@ var roundScore = 0;
 
 //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 
-
+// Програм эхлэхэд бэлтгэе.
 document.getElementById("score-0").textContent = 0;
 document.getElementById("score-1").textContent = 0;
 document.getElementById("current-0").textContent = 0;
 document.getElementById("current-1").textContent = 0;
 
-//
 var diceDom = document.querySelector('.dice');
 diceDom.style.display = 'none'
 
+// Шоог шидэх эвент листенер
 document.querySelector('.btn-roll').addEventListener('click', function shooShid() {
+    // 1-6 доторх санамсаргүй нэг тоог гаргаж авна.
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+    // Шооны зургийг вэб дээр гаргаж ирнэ.
     diceDom.style.display = 'block'
+        // Буусан санамсаргүй тоонд харгалзах шооны зургийг вэб дээр гаргаж ирнэ.
     diceDom.src = 'dice-' + diceNumber + '.png';
 
-    // alert('Шоо буулаа: ' + diceNumber);
+    // Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+    if (diceNumber !== 1) {
+        // 1-ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө.
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {
+        // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө.
+
+        // Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = 0;
+
+        // Хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго.
+        // Үгүй бол идэвхтэй тоглогчийг 0 болго.
+
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+        // Улаан цэгийг шилжүүлэх
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // Шоог түр алга болгоно
+        diceDom.style.display = 'none'
+            // if (activePlayer === 0) {
+            //     activePlayer === 1;
+            // } else {
+            //     activePlayer === 0;
+            // }
+    }
 });
-
-
-
-// console.log('Шоо :' + dice);
