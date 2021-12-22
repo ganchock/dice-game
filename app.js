de70ec12674aa423048e138d07e7e59caca876b0
ganchock <ganchock@yahoo.com>
2021-12-22 21:49:21 +0800

    shoo shideh togloomond oorchlolt hiiv.

---
+++ app.js
@@ -0,0 +1,20 @@
+//Тоглогчийн ээлжийг хадгалах хувьсагч
+var activePlayer = 0;
+
+//Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+
+//Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч 
+var roundScore = 0;
+
+//Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+var dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#score-0').textContent = 0;
+document.querySelector('#score-1').textContent = 0;
+
+document.querySelector('#current-0').textContent = 0;
+document.querySelector('#current-1').textContent = 0;
+
+document.querySelector('.dice').style.display = 'none'
+console.log('Шоо :' + dice);
