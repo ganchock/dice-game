aeaffc4db927d985a6ca43c1154e3236ff29efdd
ganchock <ganchock@yahoo.com>
2021-12-22 22:10:02 +0800

    shoo shiddeg boloh

--- app.js
+++ app.js
@@ -8,13 +8,25 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-document.querySelector('.dice').style.display = 'none'
-console.log('Шоо :' + dice);
+//
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = 'none'
+
+document.querySelector('.btn-roll').addEventListener('click', function shooShid() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = 'block'
+    diceDom.src = 'dice-' + diceNumber + '.png';
+
+    // alert('Шоо буулаа: ' + diceNumber);
+});
+
+
+
+// console.log('Шоо :' + dice);
